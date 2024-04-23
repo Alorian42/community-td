7ba35d4a37db1f935862415c002f24ff90b2ae3a
Alorian42 <alorian42@gmail.com>
2024-04-23 19:12:09 +0200

    00 Feature - Alorian42 Init

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,23 @@
-import { Engine } from "excalibur";
-import { Player } from "./player";
-import { loader } from "./resources";
+import { Engine } from 'excalibur';
+import Player from './player';
+import { loader } from './resources';
 
 class Game extends Engine {
-    constructor() {
-      super({width: 800, height: 600});
-    }
-    initialize() {
-      const player = new Player();
-      this.add(player);
+	constructor() {
+		super({ width: 800, height: 600 });
+	}
 
-      this.start(loader);
-    }
-  }
-  
-  export const game = new Game();
-  game.initialize();
+	public initialize(): void {
+		const player = new Player();
+		this.add(player);
+
+		this.start(loader).catch(error => {
+			console.error('Error loading game resources:', error);
+		});
+	}
+}
+
+const game = new Game();
+game.initialize();
+
+export default game;
--- src/player.ts
+++ src/player.ts
@@ -1,19 +1,19 @@
-import { Actor, vec } from "excalibur";
-import { Resources } from "./resources";
+import { Actor, vec } from 'excalibur';
+import { Resources } from './resources';
 
-export class Player extends Actor {
-  constructor() {
-    super({
-      pos: vec(150, 150),
-      width: 100,
-      height: 100
-    });
-  }
+export default class Player extends Actor {
+	constructor() {
+		super({
+			pos: vec(150, 150),
+			width: 100,
+			height: 100,
+		});
+	}
 
-  onInitialize() {
-    this.graphics.add(Resources.Sword.toSprite());
-    this.on('pointerup', () => {
-      alert('yo');
-    });
-  }
+	public onInitialize(): void {
+		this.graphics.add(Resources.Sword.toSprite());
+		this.on('pointerup', () => {
+			console.log('yo');
+		});
+	}
 }
